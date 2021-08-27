7de409ddf014df5b34b2bf670dc34c001b73c48e
Hong <hkhong91@gmail.com>
2021-08-27 21:07:43 +0900

    text

--- src/socket/chat.io.js
+++ src/socket/chat.io.js
@@ -1,15 +1,19 @@
-const io = require('socket.io')()
+const io = require('socket.io')({
+  cors: {
+    origin: 'http://localhost:3000',
+  }
+})
 
 io.on("connection", (socket) => {
-  console.log('user connected.' + socket.id)
+  console.log(`user connected. ${socket.id}`)
 
   socket.on('message', (text) => {
-    console.log('Message received: ' + text)
+    console.log(`Message received: ${text}`)
     io.emit('message', text)
   })
 
-  socket.on('disconnect', function () {
-    console.log('user disconnected.')
+  socket.on('disconnect', () => {
+    console.log(`user disconnected. ${socket.id}`)
   })
 })
 
